370f7d3b247dbade4bf8fb8558571790a9a69ce0
Dipak <dppote37@gmail.com>
2025-10-18 12:10:36 +0530

    number updated at sticky form

--- components/StickyForm.tsx
+++ components/StickyForm.tsx
@@ -33,7 +33,7 @@ export default function StickyForm({ isSubmitted, setIsSubmitted }: isSubmitProp
         setPhone("");
         setEmail("");
 
-        const whatsappNumber = "9604276698" // <-- REPLACE WITH YOUR NUMBER
+        const whatsappNumber = "8237311365" // <-- REPLACE WITH YOUR NUMBER
         const message = `Hi, I want to enquire about dosti estates.%0AName: ${name}%0AEmail: ${email}%0APhone: ${phone}`
         window.open(`https://wa.me/${whatsappNumber}?text=${message}`, "_blank");
 
